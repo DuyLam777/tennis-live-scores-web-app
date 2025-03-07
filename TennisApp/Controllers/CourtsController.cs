d6e2d3fe841765340cab3b3928b98c859067aaf5
Luka Ojdanic <luka.ojdanic@student.kdg.be>
2025-03-07 19:49:20 +0100

    retrieve courts and players api controllers

---
+++ TennisApp/Controllers/CourtsController.cs
@@ -0,0 +1,27 @@
+// Controllers/CourtsController.cs
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TennisApp.Data;
+using TennisApp.Models;
+
+namespace TennisApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourtsController : ControllerBase
+    {
+        private readonly TennisAppContext _context;
+
+        public CourtsController(TennisAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/courts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Court>>> GetCourts()
+        {
+            return await _context.Court.ToListAsync();
+        }
+    }
+}
---
+++ TennisApp/Controllers/PlayersController.cs
@@ -0,0 +1,27 @@
+// Controllers/PlayersController.cs
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TennisApp.Data;
+using TennisApp.Models;
+
+namespace TennisApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayersController : ControllerBase
+    {
+        private readonly TennisAppContext _context;
+
+        public PlayersController(TennisAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/players
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Player>>> GetPlayers()
+        {
+            return await _context.Player.ToListAsync();
+        }
+    }
+}
